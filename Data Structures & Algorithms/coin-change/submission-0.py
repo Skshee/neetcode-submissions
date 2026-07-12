956c8bfa805aca34535b0db0a72f24b3a7b22e02
Skshee <suvanshee@gmail.com>
2026-07-12 10:38:26 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        def dp(i):
+            if i == 0:
+                return 0
+            if i < 0:
+                return float('inf')
+            if i in memo:
+                return memo[i]
+
+            memo[i] = 1 + min(dp(i - coin) for coin in coins)
+            return memo[i]
+
+        memo = {}      
+        ways = dp(amount)
+        if ways != float('inf'):
+            return ways
+        else:
+            return -1
+        
