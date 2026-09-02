8c80e1f2b27e008f3eeea4323b5f71243cfd1434
Skshee <suvanshee@gmail.com>
2026-09-03 00:18:00 +0530

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        perm_s1 = [0] * 26
+        perm_s2 = [0] * 26
+        n = len(s1)
+        m = len(s2)
+
+        if n > m:
+            return False
+
+        for char in s1:
+            perm_s1[ord(char) - ord('a')] += 1
+        print(perm_s1)
+        
+        for i in range(n):
+            perm_s2[ord(s2[i]) - ord('a')] += 1
+        
+        if perm_s1 == perm_s2:
+            return True
+
+        for i in range(n, m):
+            perm_s2[ord(s2[i]) - ord('a')] += 1
+            perm_s2[ord(s2[i-n]) - ord('a')] -= 1
+
+            print(perm_s2)
+
+            if perm_s1 == perm_s2:
+                return True
+        return False
